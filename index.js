f458a82c68e6cb3a3a14264c128c85fade656435
Raj <desiaraj004@gmail.com>
2023-05-29 08:36:52 +0530

    Initial commit: setup directory structure and express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 8000;
+
+app.listen(port,function(err){
+   if(err){
+    console.log(`error running on server : ${err}`);
+   }
+
+   console.log(`successfully connected to sever on ${port}`);
+});
