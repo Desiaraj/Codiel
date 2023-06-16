64fa2b09bf5004ece2f7aed76c97f21b1aba116e
Raj <desiaraj004@gmail.com>
2023-06-16 06:56:25 +0530

    manual auth with signUp

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,6 +1,55 @@
+const userSchema = require('../models/user');
+
 module.exports.profile = function(req,res){
     console.log("<h1> Profile page called</h1>");
     return res.render('user',{
         "title":"User Profile"
     })
+}
+
+//create signUp action
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title:"SignUp"
+    })
+}
+
+//create signIn action
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title:"SignIn"
+    })
+}
+
+//Get the signUp data
+
+module.exports.create = function(req,res){
+    //TODO Validate input and create users in database
+    //check password and confirm password is equal
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    userSchema.findOne({email:req.body.email}).then((result)=>{
+        if(!result){
+            userSchema.create(req.body).then((user)=>{
+               console.log("Response",user);
+               return res.redirect('/users/signin');
+            }).catch((err)=>{
+               console.log("error while creating the user",err);
+               return;
+            });
+        }else{
+            console.log("user already exists");
+            return res.redirect('back');
+        }
+    }).catch((err)=>{
+        console.log("Got error while find the user");
+        return;
+    });
+}
+
+//User signIn
+module.exports.createSession = function(req,res){
+    //TODO create new session for a user
 }
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,10 @@ const userController = require('../controllers/user_controller');
 
 router.get('/profile',userController.profile);
 
+router.get('/signup',userController.signUp);
+
+router.get("/signin",userController.signIn);
+
+router.post('/create_user',userController.create);
+
 module.exports = router;
